930a4ad4f0409deb00e53bc7dd7be4a1c5dd862e
Lore1ei <harubator@gmail.com>
2023-04-20 20:21:47 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import './App.css';
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
+import {useState} from "react";
 
 function App() {
-   const todos = [
-        new Todo('Learn React'),
-        new Todo('Learn TS'),
-   ];
+    const [todos, setTodos] = useState<Todo[]>([]);
+
   //test
+
+    const addTodoHandler = (text: string) => {
+        const newTodo = new Todo(text);
+
+        setTodos(prevState => {
+            return prevState.concat(newTodo);
+        })
+    }
+
+    const removeTodoHandler = (todoId: string) => {
+        setTodos(prevState => {
+            return prevState.filter(todo => todo.id !== todoId)
+        })
+    }
+
+
   return (
     <div>
-      <Todos items={todos}/>
+        <NewTodo onAddTodo={addTodoHandler}/>
+      <Todos items={todos} removeTodo={removeTodoHandler}/>
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+import classes from './Todos.module.css'
 
-const Todos: React.FC<{items: Todo[]}> = (props) => {
+const Todos: React.FC<{items: Todo[]; removeTodo: (id: string) => void}> = (props) => {
+
+
     return(
-        <ul>
+        <ul className={classes.todos}>
             {props.items.map(item =>
-                <li key={item.id}>{item.text}</li>
+                <TodoItem onRemove={props.removeTodo.bind(null, item.id)} key={item.id} text={item.text} />
             )}
         </ul>
     )
